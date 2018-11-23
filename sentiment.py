1ef8d9f46fa0c958fb6509f996e1a47814bcd854
Wanlin Xie <wanlinx@gmail.com>
2018-11-23 13:14:27 -0500

    file where we get text and then compute sentiment

---
+++ sentiment.py
@@ -0,0 +1,15 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from bs4 import BeautifulSoup
+
+urlfile = "2017facebook.txt"
+
+testurl = "https://techcrunch.com/2017/12/21/facebook-and-universal-music-groups-new-partnership-will-allow-for-user-gen-videos-with-licensed-music-new-social-features/"
+
+if __name__ == "__main__":
+
+
+	#fr = open(urlfile,'r')
+
+	#fr.close()
+
+
